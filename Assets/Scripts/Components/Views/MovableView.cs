b5718da83959bc6c1d0da57349fb46962b7a15b9
JinnFighter <chernayamolnia@yandex.ru>
2022-08-05 02:38:50 +0300

    add: Movable and MovableView

---
+++ Assets/Scripts/Components/Views/MovableView.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace FarmingWarrior
+{
+    [Serializable]
+    public struct MovableView
+    {
+        public Transform Transform;
+        public Rigidbody Rigidbody;
+    }
+}
---
+++ Assets/Scripts/UnityComponents/Providers/MovableViewProvider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Voody.UniLeo;
+
+namespace FarmingWarrior
+{
+    public class MovableViewProvider : MonoProvider<MovableView>
+    {
+       
+    }
+}
